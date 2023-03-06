1861daf21548acf491243ccc6962d0e56a646cc1
Istvan <palocsayi@gmail.com>
2023-03-06 10:23:39 +0100

    init

---
+++ sv3-halado-2023-jvjbf-felezovizsga/src/main/java/org/training360/students/StudentService.java
@@ -0,0 +1,41 @@
+package org.training360.students;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalDouble;
+
+public class StudentService {
+
+    private StudentRepository repository;
+
+    public StudentService(StudentRepository repository) {
+        this.repository = repository;
+    }
+
+    public Student saveNewStudent(Student student) {
+        return repository.saveNewStudent(student);
+    }
+
+    public double calculateStudentAverageBySubject(String studentName, String subject) {
+        Optional<Student> actual = repository.findStudentByName(studentName);
+        if (actual.isEmpty()) {
+            throw new IllegalArgumentException("Cannot find student with this name: " + studentName);
+        }
+        Student student = actual.get();
+        return calculateAverage(student.getGradesOfSubject(subject));
+    }
+
+    private double calculateAverage(List<Integer> grades) {
+        OptionalDouble result = grades.stream()
+                .mapToDouble(g -> g)
+                .average();
+        return result.isPresent() ? result.getAsDouble() : 0.0;
+    }
+
+    public List<Student> findStudentsWithMoreGradesThan(int amount) {
+        List<Student> students = repository.findAllStudents();
+        return students.stream()
+                .filter(s -> s.countNumberOfGrades() > amount)
+                .toList();
+    }
+}
